1cbeb6d6d00ff30f6037cfbf3a37967efc0d4ac0
isaac <isaac@nicofun.club>
2016-07-02 15:14:05 +0900

    Fu*k git

--- www/resources/views/welcome.blade.php
+++ www/resources/views/welcome.blade.php
@@ -2,44 +2,44 @@
 <html>
     <head>
         <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
+        <base href="https://polygit.org/components/">
+        <script src="webcomponentsjs/webcomponents-lite.js"></script>
+        <link rel="import" href="app-layout/app-drawer-layout/app-drawer-layout.html">
+        <link rel="import" href="app-layout/app-drawer/app-drawer.html">
+        <link rel="import" href="app-layout/app-toolbar/app-toolbar.html">
+        <link rel="import" href="app-layout/app-header-layout/app-header-layout.html">
+        <link rel="import" href="app-layout/app-header/app-header.html">
+        <link rel="import" href="app-layout/app-scroll-effects/app-scroll-effects.html">
+        <link rel="import" href="app-layout/app-toolbar/app-toolbar.html">
+        <link rel="import" href="paper-icon-button/paper-icon-button.html">
+        <link rel="import" href="iron-icons/iron-icons.html">
+        <link rel="import" href="app-layout/demo/sample-content.html">
+        <style is="custom-style">
             body {
                 margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
             }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
+            app-toolbar {
+                background-color: #1E88E5;
+                font-family: 'Roboto', Helvetica, sans-serif;
+                color: white;
+                --app-toolbar-font-size: 24px;
             }
         </style>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
+        <app-drawer-layout>
+            <app-drawer>
+                <app-toolbar>Getting Started!</app-toolbar>
+            </app-drawer>
+            <app-header-layout>
+                <app-header reveals effects="waterfall">
+                    <app-toolbar>
+                        <paper-icon-button icon="menu" drawer-toggle></paper-icon-button>
+                        <div main-title>Schwul</div>
+                    </app-toolbar>
+                </app-header>
+                <sample-content size="10"></sample-content>
+            </app-header-layout>
+        </app-drawer-layout>
     </body>
 </html>
